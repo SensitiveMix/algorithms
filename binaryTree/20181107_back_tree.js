ba9449caa3423e27cf8c0bff7088629a822d76e7
Jack Sun <SensitiveMix@users.noreply.github.com>
2019-01-14 18:29:39 +0800

    chore: update binaryTree

--- binaryTree/20181107_back_tree.js
+++ binaryTree/20181107_back_tree.js
@@ -3,40 +3,6 @@
  * @param {*} key
  */
 
-function Node (key) {
-  this.key = key
-  this.left = null
-  this.right = null
-}
-
-var root = null
-function BinaryTree () {
-  this.insert = function (key) {
-    const newNode = new Node(key)
-    if (root === null) {
-      root = newNode
-    } else {
-      insertNode(root, newNode)
-    }
-  }
-
-  this.backTree = function (root, callback) {
-    backTreeNode(root, callback)
-  }
-}
-
-function backTreeNode (node, callback) {
-  if (node !== null) {
-    backTreeNode(node.left, callback)
-    backTreeNode(node.right, callback)
-    callback(node.key)
-  }
-}
-
-function callback (key) {
-  console.log(key)
-}
-
 function insertNode (node, newNode) {
   if (newNode.key < node.key) {
     if (node.left === null) {
@@ -52,12 +18,47 @@ function insertNode (node, newNode) {
     }
   }
 }
-
+// 批量插入二叉树节点
 const nodes = [8, 3, 10, 1, 6, 14, 4, 7, 13]
 const binaryTree = new BinaryTree()
+nodes.forEach(node => { binaryTree.insert(node) })
 
-nodes.forEach(node => {
-  binaryTree.insert(node)
-})
+function Node (key) {
+  this.key = key
+  this.left = null
+  this.right = null
+}
 
+var root = null
+function BinaryTree () {
+  // 构建平衡二叉树
+  this.insert = function (key) {
+    const newNode = new Node(key)
+    // 插入叶子节点
+    if (root === null) {
+      root = newNode
+    } else {
+      // 插入子节点
+      insertNode(root, newNode)
+    }
+  }
+  // 后续遍历平衡二叉树
+  this.backTree = function (root, callback) {
+    backTreeNode(root, callback)
+  }
+}
+
+function backTreeNode (node, callback) {
+  if (node !== null) {
+    backTreeNode(node.left, callback)
+    backTreeNode(node.right, callback)
+    callback(node.key)
+  }
+}
+
+function callback (key) {
+    console.log(key)
+}
+
+// 后续遍历节点
 binaryTree.backTree(root, callback)
