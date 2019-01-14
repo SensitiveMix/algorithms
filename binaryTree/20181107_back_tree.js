3c819d58b4da5e262f0ac08e634427e34d3706cf
Jack Sun <SensitiveMix@users.noreply.github.com>
2019-01-14 19:35:25 +0800

    chore: update tree

--- binaryTree/20181107_back_tree.js
+++ binaryTree/20181107_back_tree.js
@@ -1,15 +1,19 @@
 /**
- * 后续遍历，先访问左子树，再访问右子树，最后访问根节点，输出对象。
+ * 后序遍历，先访问左子树，再访问右子树，最后访问根节点，输出对象。
+ * 前序遍历，先访问根节点，再访问左子树，最后访问右子树，输出对象。
+ * 中序遍历，先访问右子树，再访问左子树，最后访问根节点，输出对象。
  * @param {*} key
  */
 
 function insertNode (node, newNode) {
+  // 递归遍历左子树
   if (newNode.key < node.key) {
     if (node.left === null) {
       node.left = newNode
     } else {
       insertNode(node.left, newNode)
     }
+  // 递归遍历右子树
   } else {
     if (node.right === null) {
       node.right = newNode
@@ -17,16 +21,6 @@ function insertNode (node, newNode) {
       insertNode(node.right, newNode)
     }
   }
-}
-// 批量插入二叉树节点
-const nodes = [8, 3, 10, 1, 6, 14, 4, 7, 13]
-const binaryTree = new BinaryTree()
-nodes.forEach(node => { binaryTree.insert(node) })
-
-function Node (key) {
-  this.key = key
-  this.left = null
-  this.right = null
 }
 
 var root = null
@@ -46,6 +40,17 @@ function BinaryTree () {
   this.backTree = function (root, callback) {
     backTreeNode(root, callback)
   }
+}
+
+// 批量插入二叉树节点
+const nodes = [8, 3, 10, 1, 6, 14, 4, 7, 13]
+const binaryTree = new BinaryTree()
+nodes.forEach(node => { binaryTree.insert(node) })
+
+function Node (key) {
+  this.key = key
+  this.left = null
+  this.right = null
 }
 
 function backTreeNode (node, callback) {
